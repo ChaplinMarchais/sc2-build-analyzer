55ab1656dff286187c1adeee4674986581d726a5
Chaplin Marchais <chaplin.m@fluidconnect-app.com>
2021-07-22 20:33:47 -0700

    Continued Working on Navigation Views #1 Added the conditional rendering for the back button on the navigation, to only display if you have moved forward in the view context. This was accomplished by wrapping a Frame inside of the NavigationView component, and binding it's 'CanGoBack' property to the back button's enabled property.
    Also modified the logic which handles rendering the content to the NavView, to instead correctly render the view within the newly added Frame. (This allows us to have full metadata about the sequence of pages which have been viewed, in order to determine logic such as the conditional back button rendering). The new logic now programmatically resolves the proper uwp page from the '/Views' directory according to the 'Tag' property present on the NavigationViewItem entries.

---
+++ sc2-build-analyzer/libs/sc2-build-analyzer.core/Uwp/NavigationException.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Runtime.Serialization;
+
+namespace sc2_build_analyzer.core.Uwp
+{
+    [Serializable]
+    public class NavigationException : Exception
+    {
+        private string message;
+        private string name;
+
+        public NavigationException()
+        {
+        }
+
+        public NavigationException(string message) : base(message)
+        {
+        }
+
+        public NavigationException(string message, string name)
+        {
+            this.message = message;
+            this.name = name;
+        }
+
+        public NavigationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NavigationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- sc2-build-analyzer/sc2-build-analyzer/MainPage.xaml.cs
+++ sc2-build-analyzer/sc2-build-analyzer/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,9 +13,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Animation;
+using sc2_build_analyzer.core.Uwp;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
+#nullable enable
 namespace sc2_build_analyzer
 {
     /// <summary>
@@ -22,9 +26,44 @@ namespace sc2_build_analyzer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const string _viewTypePrefix = @"NavigationView.Views";
+
+        private NavigationViewItem _lastViewItem;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
+
+        private void NavigationView_OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (!(args.InvokedItemContainer is NavigationViewItem item) || item == _lastViewItem)
+                return;
+
+            var selectedItemTag = item.Tag?.ToString() ?? "StngsView";
+            if (!TryNavigateToView(selectedItemTag)) return;
+
+            _lastViewItem = item;
+        }
+
+        private bool TryNavigateToView(string selectedItemTag) 
+            => ResolveViewFromTag(selectedItemTag, out var view) 
+                    && ContentFrame.Navigate(view, null, new EntranceNavigationTransitionInfo());
+
+        private bool ResolveViewFromTag(string viewTag, out Type? viewType)
+        {
+            viewType = Assembly.GetExecutingAssembly()?.GetType($"{_viewTypePrefix}.{viewTag}");
+
+            return viewType != null;
+        }
+        
+        private void NavigationView_OnBackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (ContentFrame.CanGoBack)
+                ContentFrame.GoBack();
+        }
+
+        private void ContentFrame_OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+            => throw new NavigationException(e.Exception.Message, e.SourcePageType.Name);
     }
 }
